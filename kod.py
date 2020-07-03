4b12ec8d01ed6e24251d24edc8d1a811f26da1ae
Szymon <malpkamalpka58@gmail.com>
2020-07-03 20:40:47 +0200

    zmiany

---
+++ kod.py
@@ -0,0 +1,15 @@
+a = input("Jak masz na imię?")
+b = input("Jak masz na nazwisko?")
+c = input("Ile masz lat?")
+
+iin = a + b
+
+print("Twoje imię i nazwisko to: ")
+print(iin)
+
+wiek = c
+
+print("Twój wiek: ")
+
+
+print(wiek)
